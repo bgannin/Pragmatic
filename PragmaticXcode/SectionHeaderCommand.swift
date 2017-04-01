5a4bd4e5ebcaaa3d6d91d62d484f73b4a1c543ed
Brian Ganninger <bganninger@atlassian.com>
2017-04-01 03:16:26 -0700

    Fixes #8 Add warning pragma entry

--- PragmaticXcode/SectionHeaderCommand.swift
+++ PragmaticXcode/SectionHeaderCommand.swift
@@ -10,18 +10,7 @@ class SectionHeaderCommand: NSObject, XCSourceEditorCommand
 {
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void
 	{
-		if (invocation.buffer.selections.count > 0 && invocation.buffer.lines.count > 0)
-		{
-			let selection: XCSourceTextRange = invocation.buffer.selections.firstObject as! XCSourceTextRange
-			let index = selection.start.line
-			let headerString = "#pragma mark -  -"
-			let updatedSelection = XCSourceTextRange.init(start: XCSourceTextPosition.init(line: index, column: 15),
-			                                              end: XCSourceTextPosition.init(line: index, column: 15))
-			
-			invocation.buffer.lines.insert(headerString, at: index)
-			invocation.buffer.selections.removeAllObjects()
-			invocation.buffer.selections.add(updatedSelection)
-		}
+		insertEditableLine(invocation: invocation, contents: "#pragma mark -  -", editPosition: 15)
 		
 		completionHandler(nil)
     }
